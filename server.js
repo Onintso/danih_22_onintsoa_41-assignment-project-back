5c5064b19729c944084bd08bac4c8d5b5caaa1ee
Onintso <60614002+Onintso@users.noreply.github.com>
2023-06-17 17:53:08 +0200

    rectif server.js

--- server.js
+++ server.js
@@ -63,19 +63,27 @@ app.route(prefix + '/subject/:id')
   .get(subject.getSubject);
 
 app.route(prefix + '/users')
-  .post(user.registerUser)
-  .get(user.verifyToken, function(req, res) {
-    User.findById(req.userId, { password: 0 }, function (err, user) {
-      if (err) return res.status(500).send("There was a problem finding the user.");
-      if (!user) return res.status(404).send("No user found.");
-  
-      res.status(200).send(user);
-    });
-    
-  });
+  .post(user.registerUser);
+ 
+app.route(prefix + '/user/:role')
+  .post(user.getUsersByRole);
+
+app.route(prefix + '/user/:id')
+  .post(user.getUsersById);
 
 app.route(prefix + '/login')
   .post(user.login);
+
+app.route(prefix + '/connected')
+.get(user.verifyToken, function(req, res) {
+  User.findById(req.userId, { password: 0 }, function (err, user) {
+    if (err) return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
+
+    res.status(200).send(user);
+  });
+  
+});
 
 app.route(prefix + '/logout')
   .get(user.logout);

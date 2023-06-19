b92bd590c8d16a87c9d1608a18382310a5d07942
Onintso <60614002+Onintso@users.noreply.github.com>
2023-06-19 19:58:01 +0200

    modif cors

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
